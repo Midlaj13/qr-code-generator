7e604dfb49ac125b7ca9cce369f287bc87649844
Midlaj13 <muhammedmidlaju@gmail.com>
2021-07-22 22:41:11 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import qrcode
+import image
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=5
+)
+d=input("enter the data to convert")
+
+data=d
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black",back_color="white")
+f=input("file name without extension")
+k=".png"
+img.save(f+k)
